611919031b5aa582e9ac32e25836c4367914cef2
AngMur <rodolfoyeih@gmail.com>
2024-09-21 11:43:54 -0600

    Loader

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+// Esperar a que la página cargue completamente
+window.addEventListener("load", function() {
+    // Ocultar el loader
+    document.getElementById("loader").style.display = "none";
+    
+    // Mostrar el contenido de la página
+    document.getElementById("content").style.display = "block";
+});
